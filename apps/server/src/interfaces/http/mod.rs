c7b54135012cf54b6d2a409c3f9c0a1a5d9206de
Juan Simancas <dev.juansimancas@proton.me>
2025-05-15 11:40:31 -0500

    add spa serving

--- apps/server/src/interfaces/http/mod.rs
+++ apps/server/src/interfaces/http/mod.rs
@@ -1,7 +1,16 @@
 pub mod dtos;
 pub mod handlers;
 
-use actix_web::web;
+use actix_files::{Files, NamedFile};
+use actix_web::{HttpRequest, HttpResponse, Result, web};
+
+async fn not_found(req: HttpRequest) -> Result<HttpResponse> {
+    let file = NamedFile::open("./apps/client/dist/404.html")?;
+    Ok(file
+        .use_last_modified(true)
+        .set_status_code(actix_web::http::StatusCode::NOT_FOUND) // TODO: Search a form to use a non deprecated function
+        .into_response(&req))
+}
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(
@@ -12,5 +21,12 @@ pub fn configure(cfg: &mut web::ServiceConfig) {
                 .service(handlers::user_handler::list_users)
                 .service(handlers::user_handler::delete_user),
         ),
+    );
+
+    cfg.service(
+        Files::new("/", "./apps/client/dist")
+            .index_file("index.html")
+            .use_last_modified(true)
+            .default_handler(web::to(not_found)),
     );
 }
